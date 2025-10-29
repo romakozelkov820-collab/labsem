2a3de5c5d45bd53a6ab31d23aad6e420330e23ea
romakozelkov820-collab <romakozelkov820@gmail.com>
2025-10-30 00:35:42 +0300

    Create insertion_sort.cpp

---
+++ Sem1/sorting/src/insertion_sort.cpp
@@ -0,0 +1,5 @@
+#include "../include/insertion_sort.h"
+
+// Явные инстанциации для поддержки различных типов
+template void insertionSort<int>(std::vector<int>&);
+template void insertionSort<double>(std::vector<double>&);
