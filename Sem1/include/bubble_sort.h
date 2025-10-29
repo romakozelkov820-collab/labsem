87c4880255d463d30f0fcd1ccab5bbde05064e9c
romakozelkov820-collab <romakozelkov820@gmail.com>
2025-10-30 00:30:16 +0300

    Create bubble_sort.h

---
+++ Sem1/include/bubble_sort.h
@@ -0,0 +1,17 @@
+#ifndef BUBBLE_SORT_H
+#define BUBBLE_SORT_H
+
+#include <vector>
+
+template <typename T>
+void bubbleSort(std::vector<T>& arr) {
+    for (size_t i = 0; i < arr.size() - 1; ++i) {
+        for (size_t j = 0; j < arr.size() - i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+#endif
