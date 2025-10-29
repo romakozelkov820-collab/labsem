0609edcbf812bcc56c346ed66d43152ebe323c68
romakozelkov820-collab <romakozelkov820@gmail.com>
2025-10-30 00:25:53 +0300

    Create selection_sort.h

---
+++ Sem1/sorting/include/selection_sort.h
@@ -0,0 +1,21 @@
+#ifndef SELECTION_SORT_H
+#define SELECTION_SORT_H
+
+#include <vector>
+
+template <typename T>
+void selectionSort(std::vector<T>& arr) {
+    for (size_t i = 0; i < arr.size() - 1; ++i) {
+        size_t minIndex = i;
+        for (size_t j = i + 1; j < arr.size(); ++j) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i) {
+            std::swap(arr[i], arr[minIndex]);
+        }
+    }
+}
+
+#endif
