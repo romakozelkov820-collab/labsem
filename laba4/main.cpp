1b5255223cd07a7b4fd80c566493a86c6a6f502f
romakozelkov820-collab <romakozelkov820@gmail.com>
2025-12-11 00:13:36 +0300

    Update main.cpp

--- laba4/main.cpp
+++ laba4/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#include "include/double_list.h"
+#include "DoubleList.h"
 
 int main() {
     DoubleList<int> lst;
 
-    lst.push_back(7);
-    lst.push_back(2);
-    lst.push_back(9);
+    lst.push_back(5);
     lst.push_back(1);
+    lst.push_back(3);
+    lst.push_back(7);
 
     std::cout << "Исходный список: ";
     lst.print();
