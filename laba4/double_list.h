66a193b4ba5dfe31cd68c8a7e475ff5468d41b80
romakozelkov820-collab <romakozelkov820@gmail.com>
2025-12-11 00:12:08 +0300

    Update double_list.h

--- laba4/double_list.h
+++ laba4/double_list.h
@@ -3,10 +3,7 @@
 
 #include <iostream>
 #include <vector>
-
-// Вперёд объявляем сортировки
-#include "shell_sort.h"
-#include "quick_sort.h"
+#include "processor.h"
 
 template<typename T>
 class DoubleList {
@@ -23,17 +20,17 @@ private:
     size_t count;
 
 public:
-    DoubleList() : head(nullptr), tail(nullptr), count(0) {}
+    DoubleList();
     ~DoubleList();
 
     void push_back(const T& value);
     void print() const;
 
-    // Методы сортировки
+    // Требуемое по заданию — сортировки
     void shell_sort();
     void quick_sort();
 };
 
-#include "../src/double_list.cpp"
+#include "DoubleList.cpp"
 
 #endif
