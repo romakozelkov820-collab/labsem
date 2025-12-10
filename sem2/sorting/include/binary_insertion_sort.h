853f2456ab2c7aee55e04b3b111094cac087083c
romakozelkov820-collab <romakozelkov820@gmail.com>
2025-12-10 22:01:02 +0300

    Create binary_insertion_sort.h

---
+++ sem2/sorting/include/binary_insertion_sort.h
@@ -0,0 +1,11 @@
+#ifndef BINARY_INSERTION_SORT_H
+#define BINARY_INSERTION_SORT_H
+
+#include <vector>
+
+template <typename T>
+void binary_insertion_sort(std::vector<T>& arr);
+
+#include "../src/binary_insertion_sort.cpp"
+
+#endif
