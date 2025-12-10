1a4908a990f9d776ae95b5e1ba8bb6b5ae1bf49f
romakozelkov820-collab <romakozelkov820@gmail.com>
2025-12-11 00:20:58 +0300

    Update processor.h

--- Laba3/processor.h
+++ Laba3/processor.h
@@ -2,29 +2,22 @@
 #define PROCESSOR_H
 
 #include <cctype>
-
-// remove all digit characters '0'..'9' from list
 template <typename List>
 void remove_digits(List& lst) {
     for (char d = '0'; d <= '9'; ++d) {
         lst.remove(d);
     }
 }
-
-// double '+' and '-' characters by inserting duplicate after each occurrence
 template <typename List>
 void double_signs(List& lst) {
-    // iterate with iterator and maintain index for insert()
     auto it = lst.begin();
     int idx = 0;
     while (it != lst.end()) {
         char c = *it;
         if (c == '+' || c == '-') {
-            // insert duplicate at idx+1
             lst.insert(idx + 1, c);
-            // advance iterator twice: to inserted, then to the element after inserted
-            ++it; // now points to the inserted duplicate
-            ++it; // now points to next original element (or end)
+            ++it; 
+            ++it; 
             idx += 2;
         } else {
             ++it;
@@ -32,8 +25,6 @@ void double_signs(List& lst) {
         }
     }
 }
-
-// reverse list if size is even
 template <typename List>
 void reverse_if_even(List& lst) {
     if (lst.size() % 2 == 0) lst.reverse();
@@ -46,4 +37,4 @@ void process_string_list(List& lst) {
     reverse_if_even(lst);
 }
 
-#endif // PROCESSOR_H
+#endif
